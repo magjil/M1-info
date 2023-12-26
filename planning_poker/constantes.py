597a997a67054f95d2d2342564f25bfa59093f4d
ElectroWave182 <150686082+ElectroWave182@users.noreply.github.com>
2023-12-26 04:41:51 +0100

    Update constantes.py

--- planning_poker/constantes.py
+++ planning_poker/constantes.py
@@ -5,24 +5,40 @@ import pygame_menu
 from pathlib import Path
 
 
-# Données
-cheminDonnees = str (Path (__file__).resolve ().parent) + "/donnees"
+# Dossiers
+cheminConfigs = str (Path (__file__).resolve ().parent) + "/configs"
+cheminImages = str (Path (__file__).resolve ().parent) + "/images/"
+nomCartes = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "5",
+    "8",
+    "13",
+    "20",
+    "40",
+    "100",
+    "cafe",
+    "interro"
+]
 
 # Couleurs
-ROUGE   = (150, 0  , 0  )
-VERT    = (0  , 150, 0  )
-BLEU    = (0  , 0  , 150)
-CYAN    = (0  , 200, 200)
-MAGENTA = (200, 0  , 200)
-JAUNE   = (200, 200, 0  )
-BEIGE   = (239, 231, 211)
-NOIR    = (0  , 0  , 0  )
-BLANC   = (255, 255, 255)
+ROUGE     = (150, 0  , 0  )
+VERT      = (0  , 150, 0  )
+BLEU      = (0  , 0  , 150)
+CYAN      = (0  , 200, 200)
+MAGENTA   = (200, 0  , 200)
+JAUNE     = (200, 200, 0  )
+BEIGE     = (239, 231, 211)
+TURQUOISE = (228, 230, 246)
+NOIR      = (0  , 0  , 0  )
+BLANC     = (255, 255, 255)
 
 # Disposition
 SOLAIRE = pygame_menu.themes.THEME_SOLARIZED
 BLEUTE = pygame_menu.themes.THEME_BLUE
-LARGEUR, HAUTEUR = 700, 600
+LARGEUR, HAUTEUR = 1200, 1000
 SURFACE = pygame.display.set_mode ((LARGEUR, HAUTEUR))
 
 # Modes de jeu
